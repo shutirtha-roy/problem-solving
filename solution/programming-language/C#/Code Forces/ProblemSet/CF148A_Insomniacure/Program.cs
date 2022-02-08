24d221a1efe03224257c3cfcd5a735ec5303718f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-08 20:16:47 +0600

    Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF148A_Insomniacure/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CF148A_Insomniacure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int k = int.Parse(value);
+                int l = int.Parse(Console.ReadLine());
+                int m = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+                int d = int.Parse(Console.ReadLine());
+                int count = 0;
+
+                for(var i = 1; i <= d; i++)
+                {
+                    if(i % k != 0 && i % l != 0 && i % m != 0 && i % n != 0)
+                    {
+                        count += 1;
+                    }
+                }
+
+                int damagedDragons = d - count;
+
+                Console.WriteLine(damagedDragons);
+                
+            }
+        }
+    }
+}
