baff692d9102f45d81061e67328a592db0b8cffc
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-09 00:34:03 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF443A_AntonandLetters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CF443A_AntonandLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                string sentence = value.Substring(1, value.Length - 2);
+                int count = 0;
+
+                if(sentence == "")
+                {
+                    Console.WriteLine(count);
+                }
+                else
+                {
+                    string[] characters = sentence.Split(", ");
+
+                    HashSet<string> distinctCharacters = new HashSet<string>();
+
+                    foreach(string character in characters)
+                    {
+                        distinctCharacters.Add(character);
+                    }
+
+                    Console.WriteLine(distinctCharacters.Count);
+                }
+
+            }
+        }
+    }
+}
