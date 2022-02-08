ef39515fd5443fdad9a1467c2a0f60910bd8be4e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-08 19:44:25 +0600

    Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF200B_Drinks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CF200B_Drinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int n = int.Parse(value);
+                string[] fractionString = Console.ReadLine().Split(' ');
+
+                int[] fraction = Array.ConvertAll(fractionString, s => int.Parse(s));
+
+                double ratio = 0;
+
+                foreach(var individualFraction in fraction)
+                {
+                    ratio += individualFraction;
+                }
+
+                ratio /= fraction.Length;
+
+                Console.WriteLine(String.Format("{0:F12}", ratio));
+            }
+        }
+    }
+}
