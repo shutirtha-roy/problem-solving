6d324a8958c4538c81206b70a07472cb106c2ed6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-09 20:05:41 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF520A_Pangram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CF520A_Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int n = int.Parse(value);
+                string sentence = Console.ReadLine().ToLower();
+
+                Dictionary<char, int> valuePairs = new Dictionary<char, int>();
+
+                for(var i = (int)'a'; i <= (int)'z'; i++)
+                {
+                    valuePairs.Add((char)i, 0);
+                }
+          
+
+                foreach (char alphabet in sentence)
+                {
+                    valuePairs[alphabet] += 1;
+                }
+
+                int counter = 0;
+                foreach(var (character, count) in valuePairs)
+                {
+                    if (count == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if(counter == 1)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+            }
+        }
+    }
+}
