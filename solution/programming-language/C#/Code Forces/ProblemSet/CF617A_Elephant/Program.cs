eb946f1c01fd897e5a6e2dbe4bd6e2740df4bdf4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-12 01:08:30 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF617A_Elephant/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CF617A_Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int x = int.Parse(value);
+                int positions = 5;
+                int minSteps = 0;
+
+
+                while(positions >= 1)
+                {
+                    minSteps += x / positions;
+                    x = x % positions;
+
+                    if (x == 0)
+                    {
+                        break;
+                    }
+
+                    positions--;
+                }
+
+                Console.WriteLine(minSteps);
+            }
+        }
+    }
+}
