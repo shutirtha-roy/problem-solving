3b144bd8e7bf60f1f0a8fe48db0c3a3d404c92ac
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-12 02:42:16 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF1505A_Isitrated/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CF1505A_Isitrated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
