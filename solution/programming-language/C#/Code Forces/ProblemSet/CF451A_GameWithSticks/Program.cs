0526a7d3080f9c18254f61ad0b8777ab36e47848
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-12 17:56:10 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF451A_GameWithSticks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CF451A_GameWithSticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int[] integerValues = Array.ConvertAll(value.Split(' '), s => int.Parse(s));
+                int n = integerValues[0];
+                int m = integerValues[1];
+
+                if(n >= m)
+                {
+                    if(m % 2 == 0)
+                    {
+                        Console.WriteLine("Malvika");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Akshat");
+                    }
+                }
+                else
+                {
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine("Malvika");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Akshat");
+                    }
+                }
+            }
+        }
+    }
+}
