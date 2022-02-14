d024c95026a0013906207117c514488fa25503d3
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-15 01:26:03 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF732A_BuyaShovel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CF732A_BuyaShovel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int[] items = Array.ConvertAll(value.Split(' '), s => int.Parse(s));
+                int k = items[0];
+                int r = items[1];
+                int total = 0;
+                int i = 1;
+                while(true)
+                {
+                    total = k * i;
+
+                    if(total % 10 == r || total % 10 == 0)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+
+                    i++;
+                }
+            }
+        }
+    }
+}
