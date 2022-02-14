235dfb61b168cb818d126afc7233f4f81e90da29
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-02-14 22:05:34 +0600

    New Solution Added

---
+++ solution/programming-language/C#/Code Forces/ProblemSet/CF1637A_SortingParts/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace CF1637A_SortingParts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            while (!string.IsNullOrEmpty(value = Console.ReadLine()))
+            {
+                int t = int.Parse(value);
+
+                for (var i = 0; i < t; i++)
+                {
+                    int arrayLength = int.Parse(Console.ReadLine());
+                    int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                    int[] newArray = new int[array.Length];
+                    array.CopyTo(newArray, 0);
+
+                    Array.Sort(array);
+
+                    if (newArray.SequenceEqual(array))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+
+            }
+        }
+    }
+}
