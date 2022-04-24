62e47c715d0af4c59d67ec1f83051e0a32dfc6f1
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-04-24 12:44:19 +0600

    APRIL222D_SST added

---
+++ C++/CodeChef/APRIL222D_SST.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+
+void solve() {
+    int A, B;
+    cin >> A >> B;
+
+    int firstInvestor = A * 100 / 10;
+    int secondInvestor = B * 100 / 20;
+
+    if(firstInvestor == secondInvestor)
+    {
+    	cout << "ANY" << "\n";
+    }
+    else if(firstInvestor > secondInvestor)
+    {
+    	cout << "FIRST" << "\n";
+    }
+    else
+    {
+    	cout << "SECOND" << "\n";
+    }
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
