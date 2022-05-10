eaa10bca9d5ead2970cbb727919c377956d73141
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-11 04:46:18 +0600

    MAY221D_QUEENATTACK.cpp Added

---
+++ C++/CodeChef/MAY221D_QUEENATTACK.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+
+void solve() {
+    int N, X, Y;
+
+    cin >> N >> X >> Y;
+
+    int total = N * N;
+    int attackSides = 2 * (N - 1) + min(X - 1, Y - 1) + min(N - X, Y - 1) + min(N - X, N - Y) + min(X - 1, N - Y);
+    cout << attackSides << "\n";
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
