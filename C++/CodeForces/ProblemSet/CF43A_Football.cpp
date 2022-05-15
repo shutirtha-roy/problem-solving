1c4dfce09632c3f6f725455674ac41f35e1fbffd
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-16 00:10:14 +0600

    CF43A_Football Added

---
+++ C++/CodeForces/ProblemSet/CF43A_Football.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string a;
+
+    map<string, int> value;
+
+    while(n--)
+    {
+    	cin >> a;
+
+    	value[a] += 1;
+
+    }
+
+    int max = 0;
+    string name = "";
+
+    for(auto i : value)
+    {
+    	if(max < i.second)
+    	{
+    		max = i.second;
+    		name = i.first;
+    	}
+    }
+
+    cout << name << "\n";
+
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
