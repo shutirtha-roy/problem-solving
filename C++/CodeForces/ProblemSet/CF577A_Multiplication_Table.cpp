0d6d3260d008628364185a9f8eda62b153dd7ee8
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-19 17:56:24 +0600

    CF577A_Multiplication_Table Added

---
+++ C++/CodeForces/ProblemSet/CF577A_Multiplication_Table.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+void solve() {
+    int n, x, count = 0;
+    cin >> n >> x;
+
+    for(int i = 1; i * i <= x; i++)
+    {
+    	int firstNumber = i;
+    	int secondNumber = x / i;
+
+    	if(secondNumber > n || firstNumber * secondNumber != x)
+    		continue;
+
+    	if(firstNumber == secondNumber)
+    		count++;
+    	else
+    		count += 2;
+    }
+
+    cout << count << "\n";
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
