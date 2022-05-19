f47472f2b60a03bcff669742e364d0e61d5bed21
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-05-19 17:59:49 +0600

    1929_ConcatenationofArray Added

---
+++ C++/LeetCode/Array/1929_ConcatenationofArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int size = nums.size();
+        
+        for(int i = 0; i < size; i++)
+            nums.push_back(nums[i]);
+        
+        return nums;
+    }
+};
