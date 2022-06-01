fd8e65e2c9b9b68b91400c3064c8d60d995afdcb
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-01 18:54:24 +0600

    CF1326A_Bad_Ugly_Numbers Added

---
+++ C++/CodeForces/ProblemSet/CF1326A_Bad_Ugly_Numbers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    if(n == 1) cout << -1 << "\n";
+    else
+    {
+    	cout << 2;
+
+    	for(int i = 2; i <= n; i++)
+    	{
+    		cout << 3;
+    	}
+    	cout << "\n";
+    }
+
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
