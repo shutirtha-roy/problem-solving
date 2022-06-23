ec4983a95eb11f9e033f151cd23da4438eb335dd
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-24 03:07:24 +0600

    CF1335A_Candies_and_Two_Sisters Added

---
+++ C++/CodeForces/ProblemSet/CF1335A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    if(n > 2)
+    {
+    	if(n % 2 == 1)
+    		cout << n / 2 << "\n";
+    	else
+    		cout << n / 2 - 1 << "\n";
+    }
+    else
+    {
+    	cout << 0 << "\n";
+    }
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
