15ddb068cb3ac6ef2a18ae9e3c05c6ae469cedd5
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-24 03:21:28 +0600

    CF4C_Registration_system Added

---
+++ C#/Code Forces/ProblemSet/CF4C_Registration_system/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CF4C_Registration_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, int> database = new Dictionary<string, int>();
+
+            int n = int.Parse(Console.ReadLine());
+            while(n > 0)
+            {
+                string data = Console.ReadLine();
+
+                if (!database.ContainsKey(data))
+                { 
+                    database.Add(data, 0);
+                    Console.WriteLine("OK");
+                }
+                else
+                { 
+                    Console.WriteLine($"{data}{++database[data]}");
+                }
+
+                n--;
+            }
+        }
+    }
+}
