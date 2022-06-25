388c09f04b371a69134c9a48f733052585b41048
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-26 04:08:36 +0600

    CF1409A_Yet_Another_Two_Integers_Problem Added

---
+++ C++/CodeForces/ProblemSet/CF1409A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    int maxValue = max(a, b);
+    int minValue = min(a, b);
+
+    int count = (maxValue - minValue) / 10;
+
+    if((maxValue - minValue) % 10 != 0)
+    	count++;
+    
+    cout << count << "\n";
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
