0d273240cfd5a3e965fcfe6a72a3ead4f04f73d3
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-26 04:00:24 +0600

    CF1154A_Restoring_Three_Numbers Added

---
+++ C++/CodeForces/ProblemSet/CF1154A_Restoring_Three_Numbers.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define pll pair<int, int>
+#define vll vector<ll>
+int max(int a, int b) { return ((a > b) ? a : b); }
+int min(int a, int b) { return ((a > b) ? b : a); }
+
+
+void solve() {
+	int n = 4;
+    vector<int> xValues(n, 0);
+
+   	for(int i = 0; i < n; i++)
+   	{
+   		cin >> xValues[i];
+   	}
+
+   	sort(xValues.begin(), xValues.end());
+
+   	cout << xValues[3] - xValues[0] << " ";
+   	cout << xValues[3] - xValues[1] << " ";
+   	cout << xValues[3] - xValues[2];
+
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+    #endif
+
+	int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+
+    
+}
