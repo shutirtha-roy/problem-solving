38899d358415e03df219f566fe08e1e7c7654be4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-20 09:36:12 +0600

    206_Reverse_Linked_List Added

---
+++ C#/LeetCode/206_Reverse_Linked_List.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prevNode = null, nextNode = null;
+        ListNode currentNode = head;
+        
+        while(currentNode != null)
+        {
+            nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        
+        return prevNode;
+    }
+}
