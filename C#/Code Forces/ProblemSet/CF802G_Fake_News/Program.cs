abbf6d7ce3c279c0e94e8ab7eb10b944c1b32066
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-22 00:23:57 +0600

    CF802G_Fake_News Added

---
+++ C#/Code Forces/ProblemSet/CF802G_Fake_News/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CF802G_Fake_News
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string heidi = "heidi";
+
+            int count = 0;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (count == 5)
+                    break;
+
+                if (s[i] == heidi[count])
+                    count++;
+            }
+
+            if (count == 5)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
