ec9e94473f67721bf3430f97b57d765adcc60c32
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-21 22:42:48 +0600

    CF1230A_Dawid_and_Bags_of_Candies Added

---
+++ C#/Code Forces/ProblemSet/CF1230A_Dawid_and_Bags_of_Candies/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CF1230A_Dawid_and_Bags_of_Candies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            Array.Sort(a);
+
+            if(a[0] + a[1] + a[2] == a[3] ||  a[0] + a[1] == a[2] + a[3] || a[0] + a[2] == a[1] + a[3] || a[0] + a[3] == a[1] + a[2])
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
