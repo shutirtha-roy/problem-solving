de8564216b86122b4b1fcd05266a9ff0d5029124
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-21 22:56:37 +0600

    CF1017A_The_Rank Added

---
+++ C#/Code Forces/ProblemSet/CF1017A_The_Rank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CF1017A_The_Rank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                int[] student = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                arr[i] = student.Sum();
+            }
+
+            int thomasScore = arr[0];
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            for(int i = 0; i < n; i++)
+            {
+                if(thomasScore == arr[i])
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+                    
+            }
+        }
+    }
+}
