b3241dfc3b225ebf40ad62284872125edb5a3bfb
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-24 01:00:50 +0600

    CF867A_Between_the_Offices Added

---
+++ C#/Code Forces/ProblemSet/CF867A_Between_the_Offices/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CF867A_Between_the_Offices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string val = Console.ReadLine();
+
+            if (val[0] == 'S' && val[val.Length - 1] == 'F')
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
