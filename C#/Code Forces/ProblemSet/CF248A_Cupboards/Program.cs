8b37f86665bbd95727420affa516913af5ebb5de
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-24 00:01:25 +0600

    CF248A_Cupboards Added

---
+++ C#/Code Forces/ProblemSet/CF248A_Cupboards/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CF248A_Cupboards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftDoorCountOne = 0, rightDoorCountOne = 0, leftDoorCountZero = 0, rightDoorCountZero = 0, count = 0;
+            
+            for(int i = 0; i < n; i++)
+            {
+                int[] doors = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                leftDoorCountOne += doors[0];
+                rightDoorCountOne += doors[1];
+            }
+
+            leftDoorCountZero = n - leftDoorCountOne;
+            rightDoorCountZero = n - rightDoorCountOne;
+
+            if (rightDoorCountOne >= rightDoorCountZero)
+                count += rightDoorCountZero;
+            else
+                count += rightDoorCountOne;
+
+            if (leftDoorCountOne >= leftDoorCountZero)
+                count += leftDoorCountZero;
+            else
+                count += leftDoorCountOne;
+
+            Console.WriteLine(count);
+        }
+    }
+}
