958d5458fe5d869785fd768eab4791c720b286e9
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-24 00:39:24 +0600

    CF1281A_Suffix_Three Added

---
+++ C#/Code Forces/ProblemSet/CF1281A_Suffix_Three/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CF1281A_Suffix_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < t; i++)
+            {
+                string sentence = Console.ReadLine();
+
+                if(sentence.EndsWith("po"))
+                    Console.WriteLine("FILIPINO");
+                else if(sentence.EndsWith("desu") || sentence.EndsWith("masu"))
+                    Console.WriteLine("JAPANESE");
+                else if(sentence.EndsWith("mnida"))
+                    Console.WriteLine("KOREAN");
+            }
+        }
+    }
+}
