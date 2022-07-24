9937aa05ca7459f60d82c11b225c9a29339e81d7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-24 19:03:37 +0600

    CF746A_Compote Added

---
+++ C#/Code Forces/ProblemSet/CF746A_Compote/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CF746A_Compote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            while(a >= 0)
+            {
+                if(a * 2 <= b && a * 4 <= c)
+                {
+                    count = a;
+                    break;
+                }
+
+                a--;
+            }
+
+            Console.WriteLine(count + count * 2 + count * 4);
+        }
+    }
+}
