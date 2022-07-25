1503115956a4130b7e9ca232a59ab0c525ebfaf0
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-07-25 11:57:00 +0600

    CF38A_Army Added

---
+++ C#/Code Forces/ProblemSet/CF38A_Army/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CF38A_Army
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] oldD = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int a = ab[0];
+            int b = ab[1];
+
+            b--;
+            a--;
+
+            int total = 0;
+            for(int i = a; i < b; i++)
+            {
+                total += oldD[i];
+            }
+
+            Console.WriteLine(total);
+
+        }
+    }
+}
