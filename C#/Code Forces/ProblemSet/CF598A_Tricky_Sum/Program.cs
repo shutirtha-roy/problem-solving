e4e0ec7d29dd3bba2c64c8150c36e12daf44caab
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-08-31 02:35:49 +0100

    CF598A_Tricky_Sum Added

---
+++ C#/Code Forces/ProblemSet/CF598A_Tricky_Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CF598A_Tricky_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long t = long.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                long n = long.Parse(Console.ReadLine());
+                long sum = n * (n + 1) / 2;
+                long negation = 0;
+
+                for (long j = 1; j <= n; j *= 2)
+                {
+                    sum -= 2 * j;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
