ce7436878645eb81c733e60b78befd24776efd51
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-25 23:15:28 +0600

    CF1399B_Gifts_Fixing Added

---
+++ C#/Code Forces/ProblemSet/CF1399B_Gifts_Fixing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace CF1399B_Gifts_Fixing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+                long minMove = 0;
+                int aMin = a.Min();
+                int bMin = b.Min();
+                
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[j] == aMin && b[j] == bMin)
+                        continue;
+
+                    if(a[j] - aMin <= b[j] - bMin)
+                    {
+                        minMove += b[j] - bMin;
+                    }
+                    else
+                    {
+                        minMove += a[j] - aMin;
+                    }
+                }
+
+                Console.WriteLine(minMove);
+            }
+        }
+    }
+}
