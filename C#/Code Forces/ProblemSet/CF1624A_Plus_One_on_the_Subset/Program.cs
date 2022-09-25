9e791ddbcb8a8113d7a4b042e93a6a43d424ad01
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-25 22:32:27 +0600

    CF1624A_Plus_One_on_the_Subset Added

---
+++ C#/Code Forces/ProblemSet/CF1624A_Plus_One_on_the_Subset/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace CF1624A_Plus_One_on_the_Subset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                int maxValue = a.Max();
+                int minValue = a.Min();
+                Console.WriteLine(maxValue - minValue);
+            }
+        }
+    }
+}
