d5dee4706af15716e06ae6d0fc8f9fe29c908cdf
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-26 02:44:31 +0600

    CF1472B_Fair_Division Added

---
+++ C#/Code Forces/ProblemSet/CF1472B_Fair_Division/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CF1472B_Fair_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                int oneCount = a.Count(s => s == 1);
+                int twoCount = a.Count(s => s == 2);
+                int totalCount = oneCount + 2 * twoCount;
+
+                if(oneCount == 0 && n % 2 == 0)
+                    Console.WriteLine("YES");
+                else if(totalCount % 2 == 0 && oneCount >= 2)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
