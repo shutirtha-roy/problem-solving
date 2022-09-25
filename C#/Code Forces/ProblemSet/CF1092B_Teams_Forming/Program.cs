8c4f685b8a2e52d357b54b488443069a2f5a8c8e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-26 02:52:55 +0600

    CF1092B_Teams_Forming Added

---
+++ C#/Code Forces/ProblemSet/CF1092B_Teams_Forming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CF1092B_Teams_Forming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            Array.Sort(a);
+            int change = 0;
+
+            for(int j = 0; j < a.Length; j += 2)
+            {
+                change += a[j + 1] - a[j];
+            }
+
+            Console.WriteLine(change);
+        }
+    }
+}
