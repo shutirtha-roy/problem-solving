757c6d6b5f6a458c875fce37e0c754053d5e12c6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-27 23:33:18 +0600

    CF1371A_Magical_Sticks Added

---
+++ C#/Code Forces/ProblemSet/CF1371A_Magical_Sticks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CF1371A_Magical_Sticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if(n % 2 == 0)
+                {
+                    Console.WriteLine(n / 2);
+                }
+                else
+                {
+                    Console.WriteLine(1 + n / 2);
+                }
+                    
+            }
+        }
+    }
+}
