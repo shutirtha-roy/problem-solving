372e76e3f13a88bc30c8f942f911d8f489fe9c8f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-09-28 23:05:54 +0600

    CF1472A_Cards_for_Friends Added

---
+++ C#/Code Forces/ProblemSet/CF1472A_Cards_for_Friends/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CF1472A_Cards_for_Friends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                int w = a[0], h = a[1], n = a[2];
+                int times = 1;
+
+                while(w % 2 == 0)
+                {
+                    times *= 2;
+
+                    if (w % 2 == 0)
+                        w /= 2;
+                }
+
+                while(h % 2 == 0)
+                {
+                    times *= 2;
+
+                    if (h % 2 == 0)
+                        h /= 2;
+                }
+
+                if(n <= times)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
