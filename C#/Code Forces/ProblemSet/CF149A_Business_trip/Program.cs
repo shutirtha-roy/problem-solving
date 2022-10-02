f3f4422c614c5bbdb6a2ec918f32f67abaad1bb6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-02 17:01:41 +0600

    CF149A_Business_trip Added

---
+++ C#/Code Forces/ProblemSet/CF149A_Business_trip/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CF149A_Business_trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int count = 0;
+
+            Array.Sort(a);
+            Array.Reverse(a);
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (k <= 0)
+                    break;
+
+                k -= a[i];
+                count++;
+            }
+
+            if(k > 0)
+                Console.WriteLine(-1);
+            else
+                Console.WriteLine(count);
+        }
+    }
+}
