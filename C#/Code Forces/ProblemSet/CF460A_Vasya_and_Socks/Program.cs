3d86ca385182402f88bb2f017bd68a26c7e6064b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-02 16:53:37 +0600

    CF460A_Vasya_and_Socks Added

---
+++ C#/Code Forces/ProblemSet/CF460A_Vasya_and_Socks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CF460A_Vasya_and_Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int n = nm[0], m = nm[1];
+            
+            for(int i = 1; m * i <= n; i++)
+            {
+                n++;
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
