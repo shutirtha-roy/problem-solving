fce2c61ae1bf3fb5eb3787b79f5323556a2d98d6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-02 17:27:24 +0600

    CF977B_Two_gram Added

---
+++ C#/Code Forces/ProblemSet/CF977B_Two_gram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CF977B_Two_gram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            IDictionary<string, int> subPair = new Dictionary<string, int>();
+
+            for(int i = 0; i < s.Length; i += 1)
+            {
+                if (i + 1 == s.Length)
+                    break;
+
+                if(!subPair.ContainsKey(s.Substring(i, 2)))
+                {
+                    subPair.Add(s.Substring(i, 2), 0);
+                }
+                else
+                {
+                    subPair[s.Substring(i, 2)] += 1;
+                }
+            }
+
+            string value = subPair.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            Console.WriteLine(value);
+
+        }
+    }
+}
