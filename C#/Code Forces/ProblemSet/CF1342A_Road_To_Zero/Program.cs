41112b24721d0872cb229d3d26d62846f78147bf
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-10-06 01:19:44 +0600

    CF1342A_Road_To_Zero Added

---
+++ C#/Code Forces/ProblemSet/CF1342A_Road_To_Zero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CF1342A_Road_To_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long t = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= t; i++)
+            {
+                long[] xy = Array.ConvertAll(Console.ReadLine().Split(' '), s => long.Parse(s));
+                long x = xy[0], y = xy[1];
+                long[] ab = Array.ConvertAll(Console.ReadLine().Split(' '), s => long.Parse(s));
+                long a = ab[0], b = ab[1];
+
+                long maxValue = Math.Max(x, y), minValue = Math.Min(x, y);
+                long total = 0;
+
+                if(a * 2 >= b)
+                {
+                    total += b * minValue;
+                    maxValue -= minValue;
+                    minValue = 0;
+                    total += maxValue * a;
+                }
+                else
+                {
+                    total += minValue * a + maxValue * a;
+                }
+
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
