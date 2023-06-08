9ea7b8c81d1a8d9221b0d37f305d08520125c295
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-06-08 09:17:14 +0600

    Task 1 Added

--- C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Program.cs
+++ C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Array_Part1;
+
+
+#region Task1
+Task1 task1 = new();
+var sequence = new int[] { 10, 20, 30, 40, 50, 60 };
+var beats = new int[] { 1, 0, 0, 1, 0, 1 };
+var result = task1.PlayRight(sequence, beats);
+Console.WriteLine(string.Join(", ", result));
+#endregion
+
---
+++ C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Task1.cs
@@ -0,0 +1,34 @@
+﻿namespace Array_Part1;
+
+public class Task1
+{
+    private int CalculateTotalBeats(int[] beats)
+    {
+        return (from beat in beats
+            where beat == 1
+            select beat).Count();
+    }
+
+    private int CheckWhetherPointerExceedsLength(int pointer, int length)
+    {
+        return pointer == length
+                ? 0
+                : pointer;
+    }
+
+    public int[] PlayRight(int[] sequence, int[] beats)
+    {
+        var totalBeats = CalculateTotalBeats(beats);
+        var afterRightSequence = new int[sequence.Length];
+        var pointer = totalBeats;
+
+        for(var index = 0; index < sequence.Length; index++)
+        {
+            pointer = CheckWhetherPointerExceedsLength(pointer, sequence.Length);
+            afterRightSequence[pointer] = sequence[index];
+            pointer++;
+        }
+
+        return afterRightSequence;
+    }
+}
