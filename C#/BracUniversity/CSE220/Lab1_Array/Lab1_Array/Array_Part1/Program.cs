cca4b3de235e777ba4ea8d3935590a0164e41af6
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-06-15 08:54:27 +0600

    Task 2 Initialized

--- C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Program.cs
+++ C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Program.cs
@@ -2,10 +2,16 @@
 
 
 #region Task1
-Task1 task1 = new();
-var sequence = new int[] { 10, 20, 30, 40, 50, 60 };
-var beats = new int[] { 1, 0, 0, 1, 0, 1 };
-var result = task1.PlayRight(sequence, beats);
-Console.WriteLine(string.Join(", ", result));
+//Task1 task1 = new();
+//var sequence = new int[] { 10, 20, 30, 40, 50, 60 };
+//var beats = new int[] { 1, 0, 0, 1, 0, 1 };
+//var result = task1.PlayRight(sequence, beats);
+//Console.WriteLine(string.Join(", ", result));
 #endregion
 
+#region Task1
+Task2 task2 = new();
+var cards = new int[] { 10, 2, 30, 2, 50, 2, 2, 0, 0 };
+var resultTask2 = task2.DiscardCards(cards, 2);
+Console.WriteLine(string.Join(", ", resultTask2));
+#endregion
---
+++ C#/BracUniversity/CSE220/Lab1_Array/Lab1_Array/Array_Part1/Task2.cs
@@ -0,0 +1,20 @@
+﻿namespace Array_Part1;
+
+public class Task2
+{
+    private int[] EmptyRequiredPosition(int[] cards, int number)
+    {
+        return cards
+            .Select(card => card == number ? 0 : card)
+            .ToArray();
+    }
+
+    public int[] DiscardCards(int[] cards, int number)
+    {
+        var discardCards = EmptyRequiredPosition(cards, number);
+
+
+
+        return discardCards;
+    }
+}
