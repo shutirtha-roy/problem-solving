76b6d9aca630e92fc4b3337bddc783477f6a6254
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-06-22 17:48:28 +0600

    Task2 Initially Added

---
+++ C#/BracUniversity/CSE220/Lab3_SingleLinkedList/SingleLinkedList/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using LinkedListGenerator;
+using Task2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = new Solution();
+
+        var head = new LinkedListModel()
+            .PopulateList(new int[] { 10, 15, 34, 41, 56, 72 });
+
+        //var result = solution
+            //.CheckIfListAscending(head);
+
+        //Console.WriteLine(result);
+    }
+
+
+}
---
+++ C#/BracUniversity/CSE220/Lab3_SingleLinkedList/SingleLinkedList/Task2/Solution.cs
@@ -0,0 +1,35 @@
+﻿using LinkedListGenerator;
+
+namespace Task2;
+
+public class Solution
+{
+    private int TotalElement(ListNode head)
+    {
+        var currentNode = head;
+        var count = 0;
+
+        while (currentNode != null)
+        {
+            count++;
+            currentNode = currentNode.next;
+        }
+
+        return count;
+    }
+
+    public ListNode GetModifiedCompartmentSequence(ListNode head, int lastPos)
+    {
+        var totalElement = TotalElement(head);
+        var positionBeforeItem = totalElement - lastPos;
+
+        if(positionBeforeItem < 0)
+        {
+            return head;
+        }
+
+
+
+        return new ListNode();
+    }
+}
